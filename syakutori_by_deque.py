4b3a0541ba145e26904f2eb405b873d0e5c26f0f
Yuya Ohbayashi <yuya9445@gmail.com>
2021-11-28 09:31:16 +0900

    rename file

--- syakutori_by_deque.py
+++ syakutori_by_deque.py
@@ -1,16 +1,15 @@
 from collections import deque
 
-## 入力の受け取り
-n, k = map(int, input().split())
-a = [int(input()) for i in range(n)]
+N, Q = map(int, input().split())
+A = list(map(int, input().split()))
 
-q = deque()
-for c in a:
-    q.append(c)  ## dequeの右端に要素を一つ追加する。
+que = deque()
+for a in A:
+    que.append(a)  ## dequeの右端に要素を一つ追加する。
     (追加した要素に応じて何らかの処理を行う)
 
-    while q and (満たすべき条件の否定):
-        rm = q.popleft() ## 条件を満たさないのでdequeの左端から要素を取り除く
+    while que and (満たすべき条件の否定):
+        rm = que.popleft() ## 条件を満たさないのでdequeの左端から要素を取り除く
         (取り除いた要素に応じて何らかの処理を行う)
 
     (何らかの処理を行う。whileがbreakしたので、dequeに入っている連続部分列は条件を満たしている。特に右端の要素から左に延ばせる最大の長さになっている。)
