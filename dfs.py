32edcec50872271ab2051a53bac8e7f8caaabd02
Yuya Ohbayashi <yuya9445@gmail.com>
2021-11-30 22:37:35 +0900

    update dfs.py

--- dfs.py
+++ dfs.py
@@ -7,3 +7,15 @@ def dfs(node):
         if not visited[n_node]:
             dfs(n_node)
     return
+
+# アルゴ式
+# 頂点 v を根とする部分木を探索
+# 頂点 v の子頂点を格納した配列を chs とする
+def rec(v):
+    # 頂点 v を出力
+    print(v)
+
+    # 各子頂点を探索
+    for ch in chs:
+        # 子頂点 ch を根とした部分木を再帰的に探索
+        rec(ch)
