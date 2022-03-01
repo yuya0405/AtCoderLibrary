981d648e9aab337a5e64fe058c36605552ab167c
Yuya Ohbayashi <yuya9445@gmail.com>
2022-03-01 15:13:01 +0900

    fix union_find.py

--- union_find.py
+++ union_find.py
@@ -1,49 +1,59 @@
-# 参考：ABC231D
-
-import sys
-sys.setrecursionlimit(10**7)
+from collections import defaultdict
 
 class UnionFind():
-    # 初期化
     def __init__(self, n):
-        self.par = [-1] * n
-        self.rank = [0] * n
-        self.siz = [1] * n
+        self.n = n
+        self.parents = [-1] * n
 
-    # 根を求める
-    def root(self, x):
-        if self.par[x] == -1: return x # x が根の場合は x を返す
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
         else:
-            self.par[x] = self.root(self.par[x]) # 経路圧縮
-            return self.par[x]
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
 
-    # x と y が同じグループに属するか (根が一致するか)
-    def issame(self, x, y):
-        return self.root(x) == self.root(y)
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
 
-    # x を含むグループと y を含むグループを併合する
-    def unite(self, x, y):
-        # x 側と y 側の根を取得する
-        rx = self.root(x)
-        ry = self.root(y)
-        if rx == ry: return False # すでに同じグループのときは何もしない
-        # union by rank
-        if self.rank[rx] > self.rank[ry]: # ry 側の rank が小さくなるようにする
-            rx, ry = ry, rx
-        self.par[ry] = rx # ry を rx の子とする
-        if self.rank[rx] == self.rank[ry]: # rx 側の rank を調整する
-            self.rank[rx] += 1
-        self.siz[rx] += self.siz[ry] # rx 側の siz を調整する
-        return True
-    
-    # x を含む根付き木のサイズを求める
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
     def size(self, x):
-        return siz[root(x)]
+        return -self.parents[self.find(x)]
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+    def all_group_members(self):
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.find(member)].append(member)
+        return group_members
+
+    def __str__(self):
+        return '\n'.join(f'{r}: {m}' for r, m in self.all_group_members().items())
 
 uf = UnionFind(N)
 for a, b in AB:
-    if uf.issame(a-1, b-1):
-        print("No")
-        exit()
-    else:
-        uf.unite(a-1, b-1)
+    uf.union(a-1, b-1)
+ans = float(0)
+for i in range(N):
+    ans = max(ans, uf.size(i))
+print (ans)
